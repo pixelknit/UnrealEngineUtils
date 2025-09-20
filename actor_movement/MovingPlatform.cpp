a511a428714cbba6669a83e248da163ba084cb33
pixelknit <felipepesantez@192.168.1.111>
2025-09-20 16:54:17 +0100

    added moving platform utils

--- actor_movement/MovingPlatform.cpp
+++ actor_movement/MovingPlatform.cpp
@@ -1,6 +1,63 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
 #include "MovingPlatform.h"
 
+// Sets default values
+AMovingPlatform::AMovingPlatform()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
 
-int main(){
-  return 0;
 }
+
+// Called when the game starts or when spawned
+void AMovingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+	StartLocation = GetActorLocation();
+}
+
+// Called every frame
+void AMovingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	MovePlatform(DeltaTime);
+	RotatePlatform(DeltaTime);
+}
+
+float AMovingPlatform::GetDistanceMoved() {
+	float result = FVector::Dist(StartLocation, GetActorLocation());
+	return result;
+}
+
+void AMovingPlatform::MovePlatform(float DeltaTime) {
+
+
+	//DistanceMoved = FVector::Dist(StartLocation, CurrentLocation);
+	DistanceMoved = GetDistanceMoved();
+
+	if (DistanceMoved >= MoveDistance) {
+
+		FVector MoveDirection = PlatformVelocity.GetSafeNormal();
+		FVector NewStartLocation = StartLocation + MoveDirection * MoveDistance;
+		SetActorLocation(NewStartLocation);
+		StartLocation = NewStartLocation;
+
+		PlatformVelocity = -PlatformVelocity;
+	}
+	else {
+
+		FVector CurrentLocation = GetActorLocation();
+		CurrentLocation += (PlatformVelocity * DeltaTime);
+
+		SetActorLocation(CurrentLocation);
+	}
+}
+
+void AMovingPlatform::RotatePlatform(float DeltaTime) {
+	
+	AddActorLocalRotation(RotationVelocity * DeltaTime);
+		
+}
+
--- actor_movement/MovingPlatform.h
+++ actor_movement/MovingPlatform.h
@@ -1,6 +1,44 @@
-#include <iostream>
+// Fill out your copyright notice in the Description page of Project Settings.
 
-class AMovingPlatform{
-  UPROPERTY(EditAnywhere)
-  float MyFloat;
-}
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MovingPlatform.generated.h"
+
+UCLASS()
+class B166ER_API AMovingPlatform : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMovingPlatform();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	void MovePlatform(float DeltaTime);
+	void RotatePlatform(float DeltaTime);
+	float GetDistanceMoved();
+
+	UPROPERTY(EditAnywhere)
+	FVector PlatformVelocity = FVector(0.0f, 0.0f, 0.0f);
+
+	UPROPERTY(EditAnywhere)
+	float MoveDistance = 300.0f;
+
+	UPROPERTY(VisibleAnywhere)
+	float DistanceMoved = 0.0f;
+
+	UPROPERTY(EditAnywhere)
+	FRotator RotationVelocity;
+
+	FVector StartLocation;
+};
+
